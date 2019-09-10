c803633ce40289884aad68630789f9b392c885b4
TheJavaNerd <53121891+TheJavaNerd@users.noreply.github.com>
2019-09-10 22:59:14 +0530

    Add files via upload

---
+++ SpringBootSigninSignUpExample/src/main/java/com/cubix/serviced/ServiceD.java
@@ -0,0 +1,13 @@
+package com.cubix.serviced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceD {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceD.class, args);
+	}
+
+}
---
+++ SpringBootSigninSignUpExample/src/main/java/com/cubix/serviced/service/UserService.java
@@ -0,0 +1,9 @@
+package com.cubix.serviced.service;
+
+import com.cubix.serviced.model.User;
+
+public interface UserService {
+
+	public User findUserByEmail(String email);
+	public void saveUser(User user);
+}
